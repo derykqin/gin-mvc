6a4b21f15d70c629bb8f6c034be655856d2d3b79
derykqin <derykqin@163.com>
2023-10-18 08:54:53 +0800

    修改log.go

--- logs/log.go
+++ logs/log.go
@@ -1,1 +1,24 @@
 package logs
+
+import (
+	"io"
+	"os"
+
+	log "github.com/sirupsen/logrus"
+)
+
+func InitLogrus() {
+	log.SetFormatter(&log.JSONFormatter{}) //text formatter or json formatter
+	log.SetReportCaller(true)              //添加记录日志方法
+	log.SetLevel(log.InfoLevel)            //info level
+	writer := getFileWriter("./logs/log.txt")
+	log.SetOutput(io.MultiWriter(os.Stdout, writer)) //输出多个通道
+}
+func getFileWriter(p string) *os.File {
+	writer, err := os.OpenFile(p, os.O_WRONLY|os.O_CREATE, 0755)
+	if err != nil {
+		log.Fatalf("create file log.txt failed: %v", err)
+	}
+	return writer
+
+}
--- main.go
+++ main.go
@@ -1,25 +1,15 @@
 package main
 
 import (
+	"derykqin/gin-mvc/logs"
 	"derykqin/gin-mvc/routes"
-	"io"
-	"os"
 
 	"github.com/gin-gonic/gin"
-	log "github.com/sirupsen/logrus"
 )
 
 func init() {
 	//初始化日志框架logrus
-	log.SetFormatter(&log.JSONFormatter{}) //text formatter or json formatter
-	// log.SetReportCaller(true)//添加记录日志方法
-	log.SetLevel(log.InfoLevel) //info level
-	writer, err := os.OpenFile("./logs/log.txt", os.O_WRONLY|os.O_CREATE, 0755)
-	if err != nil {
-		log.Fatalf("create file log.txt failed: %v", err)
-	}
-	log.SetOutput(io.MultiWriter(os.Stdout, writer)) //输出多个通道
-
+	logs.InitLogrus()
 }
 
 func main() {
